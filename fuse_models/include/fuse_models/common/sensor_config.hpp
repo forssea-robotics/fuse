8665a1298a49b53032470f48b1f462fcf0bccb8c
Alaa El Jawad <ejalaa12@gmail.com>
2023-02-21 14:48:13 +0100

    enable 3d toIndex methods
    Signed-off-by: Alaa El Jawad <ejalaa12@gmail.com>

--- fuse_models/include/fuse_models/common/sensor_config.hpp
+++ fuse_models/include/fuse_models/common/sensor_config.hpp
@@ -91,6 +91,28 @@ std::enable_if_t<is_linear_2d<T>::value, size_t> toIndex(const std::string & dim
 }
 
 /**
+ * @brief Method that converts from 3D linear axis dimension names to index values
+ *
+ * This method is enabled only for variables that contain _only_ 3D linear quantities
+ *
+ * @param[in] dimension - The dimension name to convert
+ * @return the index of the enumerated dimension for that type
+ * @throws runtime_error if the dimension name is invalid
+ */
+template<typename T>
+std::enable_if_t<is_linear_3d<T>::value, size_t> toIndex(const std::string & dimension)
+{
+  auto lower_dim = boost::algorithm::to_lower_copy(dimension);
+  if (lower_dim == "x") {return static_cast<size_t>(T::X);}
+  if (lower_dim == "y") {return static_cast<size_t>(T::Y);}
+  if (lower_dim == "z") {return static_cast<size_t>(T::Z);}
+
+  throwDimensionError(dimension);
+
+  return 0u;
+}
+
+/**
  * @brief Method that converts from 2D angular axis dimension names to index values
  *
  * This method is enabled only for variables that contain _only_ 2D angular quantities
@@ -105,6 +127,34 @@ std::enable_if_t<is_angular_2d<T>::value, size_t> toIndex(const std::string & di
   auto lower_dim = boost::algorithm::to_lower_copy(dimension);
   if (lower_dim == "yaw" || lower_dim == "z") {
     return static_cast<size_t>(fuse_variables::Orientation2DStamped::YAW);
+  }
+
+  throwDimensionError(dimension);
+
+  return 0u;
+}
+
+/**
+ * @brief Method that converts from 3D angular axis dimension names to index values
+ *
+ * This method is enabled only for variables that contain _only_ 3D angular quantities
+ *
+ * @param[in] dimension - The dimension name to convert
+ * @return the index of the enumerated dimension for that type
+ * @throws runtime_error if the dimension name is invalid
+ */
+template<typename T>
+std::enable_if_t<is_angular_3d<T>::value, size_t> toIndex(const std::string & dimension)
+{
+  auto lower_dim = boost::algorithm::to_lower_copy(dimension);
+  if (lower_dim == "roll" || lower_dim == "x") {
+    return static_cast<size_t>(fuse_variables::Orientation3DStamped::Euler::ROLL);
+  }
+  if (lower_dim == "pitch" || lower_dim == "y") {
+    return static_cast<size_t>(fuse_variables::Orientation3DStamped::Euler::PITCH);
+  }
+  if (lower_dim == "yaw" || lower_dim == "z") {
+    return static_cast<size_t>(fuse_variables::Orientation3DStamped::Euler::YAW);
   }
 
   throwDimensionError(dimension);
